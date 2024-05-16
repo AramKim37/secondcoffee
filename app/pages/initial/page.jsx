e4ec1c9dd5a1bbb0ecdb23b2f6b3ab4046ee666a
AramKim37 <aramkim37@gmail.com>
2024-05-15 18:00:41 -0600

    add intial page and mainpage

---
+++ app/pages/initial/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const InitialPage = () => {
+  return (
+    <Link href="/pages/mainpage">
+      <button>Enter</button>
+    </Link>
+  );
+};
+
+export default InitialPage;
