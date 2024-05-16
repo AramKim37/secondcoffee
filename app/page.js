abf07f5d9f2bc0be6af10cf340319253fcbda2d1
AramKim37 <aramkim37@gmail.com>
2024-05-15 21:00:17 -0600

    needs gif file into intial page

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import InitialPage from "./pages/initial/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="">
       <InitialPage />
-    </main>
+    </div>
   );
 }
--- app/pages/initial/page.jsx
+++ app/pages/initial/page.jsx
@@ -3,9 +3,12 @@ import React from "react";
 
 const InitialPage = () => {
   return (
-    <Link href="/pages/mainpage">
-      <button>Enter</button>
-    </Link>
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="flex items-center justify-evenly gap-10">
+        <h1>Second Coffee</h1>
+        {/* images(coffee gif) */}
+      </div>
+    </div>
   );
 };
 
