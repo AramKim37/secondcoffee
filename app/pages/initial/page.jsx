9489571f5eb9b8e797cde4e566dac47b78dc5651
AramKim37 <aramkim37@gmail.com>
2024-05-19 16:52:19 -0600

    add background in the intial page

--- app/pages/initial/page.jsx
+++ app/pages/initial/page.jsx
@@ -7,7 +7,7 @@ import Coffee from "../../../public/images/coffee1.gif";
 const InitialPage = () => {
   const [hovered, setHovered] = useState(false);
   return (
-    <div className="w-full min-h-screen flex items-center justify-center bg-slate-300">
+    <div className="w-full min-h-screen flex items-center justify-center bg-slate-300 initial">
       <div className="flex items-center gap-10">
         <div className="flex items-center w-full h-full flex-col">
           <div className="items-center flex justify-center">
@@ -16,30 +16,16 @@ const InitialPage = () => {
             </h1>
           </div>
           <div
-            className="relative w-32 h-32 flex items-center justify-center"
+            className="relative"
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
           >
             {/* images(coffee gif) */}
-            {hovered ? (
-              <button className="bg-blue-500 text-white font-extrabold py-2 px-4 rounded transform transition-transform duration-300">
-                Enter
-              </button>
-            ) : (
-              <Image
-                src={Coffee}
-                alt="coffee"
-                className="w-32 h-32 hover:scale-[120%] ease-in-out transform transition-transform duration-300"
-              />
-            )}
-
-            {/* <span
-              className={`absolute top-20 left-28 text-white font-extrabold pl-5 ${
-                hovered ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              Ready?
-            </span> */}
+            <Image
+              src={Coffee}
+              alt="coffee"
+              className="w-32 h-32 hover:scale-[120%] ease-in-out transform transition-transform duration-300"
+            />
           </div>
         </div>
       </div>
