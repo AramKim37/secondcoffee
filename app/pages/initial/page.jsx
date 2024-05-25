8fc355d252c143e96dd141c270ec1034c9bf7e33
AramKim37 <aramkim37@gmail.com>
2024-05-25 16:30:37 -0600

    make signin page

--- app/pages/initial/page.jsx
+++ app/pages/initial/page.jsx
@@ -13,7 +13,7 @@ const InitialPage = () => {
           SECOND COFFEE
         </p>
         <Image src={Heart} alt="heart" className="w-24 h-24 animate-pulse" />
-        <Link href="/pages/mainpage">
+        <Link href="/pages/signin">
           <div className="">
             <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-yellow-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
               <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-[#452a25] rounded-md group-hover:bg-opacity-0 uppercase">
