72afdcd3f2cabdf3f9dee57cbf32043ba40b5ee5
AramKim37 <aramkim37@gmail.com>
2024-05-27 15:22:41 -0600

    add spatial css

--- app/pages/mainpage/page.jsx
+++ app/pages/mainpage/page.jsx
@@ -5,8 +5,12 @@ import Logo from "../../../public/images/logo.png";
 const MainPage = () => {
   return (
     <main className="mainbg ">
-      <div className="w-[780px] h-[780px] items-center flex justify-center">
-        <div className="spatial w-full h-full items-center flex">helllo</div>
+      <div className="flex h-full items-center justify-center">
+        <div className="w-[90%] h-[90%]">
+          <div className="spatial w-full h-full items-center flex justify-center rounded-xl">
+            helllo
+          </div>
+        </div>
       </div>
     </main>
   );
