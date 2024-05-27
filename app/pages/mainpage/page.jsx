16550fee94e195325d2fb539f79a189abf7ab1bb
AramKim37 <aramkim37@gmail.com>
2024-05-26 21:09:05 -0600

    building main page

--- app/pages/mainpage/page.jsx
+++ app/pages/mainpage/page.jsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 import React from "react";
+import Logo from "../../../public/images/logo.png";
 
 const MainPage = () => {
-  return <div>MainPage!!!</div>;
+  return (
+    <main className="mainbg ">
+      <div className="w-[780px] h-[780px] items-center flex justify-center">
+        <div className="spatial w-full h-full items-center flex">helllo</div>
+      </div>
+    </main>
+  );
 };
 
 export default MainPage;
